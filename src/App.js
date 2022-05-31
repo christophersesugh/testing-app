9e2bc6b06d3ff9aff4dc4190acbe96aa2bff84b3
Christopher Sesugh <christohybrid185@gmail.com>
2022-05-31 12:19:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Form from "./components/form";
 // import Counter from "./components/counter";
 
 function App() {
+  // form component
   const values = ({ username, password }) => {
     window.alert(`${username} ${password}`);
   };
 
-  return <Form onSubmit={values} />;
+  return (
+    <>
+      {/* <Counter /> */}
+      <Form onSubmit={values} />
+    </>
+  );
 }
 
 export default App;
